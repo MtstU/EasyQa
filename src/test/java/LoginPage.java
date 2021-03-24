bfd8144cdd280f1c67e0c91d605d35b542031ce1
MtstU <78070630+MtstU@users.noreply.github.com>
2021-03-24 16:09:37 +0300

    Added UIMap. Changed test code.

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -1,82 +1,39 @@
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.testng.Assert.fail;
+import static com.codeborne.selenide.Selenide.*;
 
 public class LoginPage {
-    private WebDriver driver;
-    private boolean acceptNextAlert = true;
-    private final StringBuffer verificationErrors = new StringBuffer();
+    //private boolean acceptNextAlert = true;
+    //private final StringBuffer verificationErrors = new StringBuffer();
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--ignore-certificate-errors");
-        driver = new ChromeDriver(options);
+        Configuration.browser = "chrome";
     }
 
     @Test
-   public void testLoginPage() throws Exception {
-        driver.get("https://app.geteasyqa.com/users/sign_in");
-        driver.findElement(By.id("user_email")).click();
-        driver.findElement(By.id("user_email")).clear();
-        driver.findElement(By.id("user_email")).sendKeys("me_se@mail.ru");
-        driver.findElement(By.id("user_password")).click();
-        driver.findElement(By.id("user_password")).clear();
-        driver.findElement(By.id("user_password")).sendKeys("123456");
-        driver.findElement(By.name("commit")).click();
-        driver.close();
+    public void testLoginPage() throws Exception {
+        open("https://app.geteasyqa.com/users/sign_in");
+        $(UIMap.login).click();
+        $(UIMap.login).clear();
+        $(UIMap.login).sendKeys("me_se@mail.ru");
+
+        $(UIMap.password).click();
+        $(UIMap.password).clear();
+        $(UIMap.password).sendKeys("123456");
+
+        $(UIMap.loginBtn).click();
 
     }
 
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
+        //close();
     }
 
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
 }
