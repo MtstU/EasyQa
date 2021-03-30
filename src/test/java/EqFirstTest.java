5d637819f15710781a75a861b42e45f85bc861f2
MtstU <78070630+MtstU@users.noreply.github.com>
2021-03-30 18:16:53 +0300

    modified test cases a little.

--- src/test/java/EqFirstTest.java
+++ src/test/java/EqFirstTest.java
@@ -12,16 +12,19 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class EqFirstTest {
 
+    UserData theUser = new UserData("me_se@mail.ru", "123456");
+    CardData issue = new CardData("test2", "issue description");
+
     @BeforeClass(alwaysRun = true)
     public void setUp() {
-        //browserCapabilities.setAcceptInsecureCerts(true);
         browser = "chrome";
         Configuration.browserSize = "1600x1000";
+
     }
 
     @Test
     public void loginAsRegisteredUser() {
-        UserData theUser = new UserData("me_se@mail.ru", "123456");
+        //UserData theUser = new UserData("me_se@mail.ru", "123456");
         LoginPage loginPage = open("https://app.geteasyqa.com/users/sign_in",LoginPage.class);
         loginPage.enterLogin(theUser.getUserEmail());
         loginPage.enterPassword(theUser.getUserPassword());
@@ -31,7 +34,7 @@ public class EqFirstTest {
 
     @Test
     public void openProjects() {
-        UserData theUser = new UserData("me_se@mail.ru", "123456");
+        //UserData theUser = new UserData("me_se@mail.ru", "123456");
         LoginPage loginPage = open("https://app.geteasyqa.com/users/sign_in",LoginPage.class);
         loginPage.enterLogin(theUser.getUserEmail());
         loginPage.enterPassword(theUser.getUserPassword());
@@ -43,8 +46,8 @@ public class EqFirstTest {
 
     @Test
     public void createCard() {
-        CardData issue = new CardData("test2", "issue description");
-        UserData theUser = new UserData("me_se@mail.ru", "123456");
+        //CardData issue = new CardData("test2", "issue description");
+        //UserData theUser = new UserData("me_se@mail.ru", "123456");
         LoginPage loginPage = open("https://app.geteasyqa.com/users/sign_in", LoginPage.class);
         loginPage.enterLogin(theUser.getUserEmail());
         loginPage.enterPassword(theUser.getUserPassword());
@@ -62,7 +65,7 @@ public class EqFirstTest {
 
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
         closeWebDriver();
     }
